a04b8556d66a90239aa98e23e4168871bce3f29a
Khan <heena-kausar-abdul-karim.khan@capgemini.com>
2025-02-20 02:59:13 +0530

    update src

--- src/test/java/com/valaxy/demo/AbstractTest.java
+++ src/test/java/com/valaxy/demo/AbstractTest.java
@@ -1,9 +1,8 @@
-package com.stalin.demo;
+package com.valaxy.demo;  // Ensure this matches your actual package
 
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,19 +20,21 @@ import java.io.IOException;
 public abstract class AbstractTest {
 
     protected MockMvc mvc;
-    @Autowired
-    WebApplicationContext webApplicationContext;
 
-    protected void setUp() {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setUp() {
         mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
+
     protected String mapToJson(Object obj) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.writeValueAsString(obj);
     }
-    protected <T> T mapFromJson(String json, Class<T> clazz)
-            throws JsonParseException, JsonMappingException, IOException {
 
+    protected <T> T mapFromJson(String json, Class<T> clazz) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.readValue(json, clazz);
     }
